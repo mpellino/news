f46686859538914e66aed71f48840d71462b7ba7
mpellino <marcopellino00@gmail.com>
2022-01-06 10:19:19 +0100

    Modify settings.py to add template folder path and redirect_url.

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'newspaper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,4 +127,5 @@ AUTH_USER_MODEL = "users.CustomUser"
 #This dotted pair describes the label of the Django app (which must be in your INSTALLED_APPS),
 # and the name of the Django model that you wish to use as your user model.
 
-
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
