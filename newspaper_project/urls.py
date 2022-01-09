ea8c7b1e6877d27d1338df39aeb52685824712d8
mpellino <marcopellino00@gmail.com>
2022-01-09 21:28:04 +0100

    Add Admin, urls, views and html.
    Admin to see the articles on the admin page.
    Urls at the project level.
    View to display the articles.
    Template.

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
-
+    path('articles/', include('articles.urls'))
 ]
