b9b5e5a4c4fbd08ee14f4d9666e8de1e7cd17d2d
mpellino <marcopellino00@gmail.com>
2022-01-10 14:03:41 +0100

    Add features to crete delete and edit articles

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from .models import Article
+from django.urls import reverse_lazy
 # Create your views here.
 
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body')
+    template_name = "article_edit.html"
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ('title', 'body', 'author')
+    template_name = "article_new.html"
+    success_url = reverse_lazy('article_list')
