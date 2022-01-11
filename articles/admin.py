ab5b109daf3526d15358dbf8ccd2426046e13a32
mpellino <marcopellino00@gmail.com>
2022-01-11 14:00:01 +0100

    Change admin page, display article & comment at the same time.

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Article, Comments
 
-# Register your models here
 
-admin.site.register(Article)
+# visualize 2 two model in a single page.
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comments
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comments) # SHOULD HAVE BEEN IN SINGULAR!!
+
+
+
