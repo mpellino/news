b40612a5e5b0375e4174ffc95588b23dc8578809
mpellino <marcopellino00@gmail.com>
2022-01-11 13:43:25 +0100

    Add model Articles to admin page

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comments
 
 # Register your models here
 
 admin.site.register(Article)
-
+admin.site.register(Comments) # SHOULD HAVE BEEN IN SINGULAR!!
